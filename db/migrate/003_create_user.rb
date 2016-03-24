23e29261948cef83ef1f3ff66f0b9a0d6ada8f5d
Tony Nguyen <tonyviet87@gmail.com>
2016-03-24 08:37:48 -0400

    added create_user

---
+++ db/migrate/003_create_user.rb
@@ -0,0 +1,14 @@
+class CreateUser < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :first
+      t.string :last
+      t.string :email
+
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
